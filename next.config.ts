579e90fa10209c3120714eb0f0c59a5363223b7e
nandakumarjobs9090-png <nandakumarjobs9090@gmail.com>
2026-09-14 01:11:11 +0530

    Add GitHub Pages deployment workflow and Next.js static export config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = '';
+if (isGithubActions) {
+  repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,6 +31,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  basePath: isGithubActions && repo ? `/${repo}` : '',
+  assetPrefix: isGithubActions && repo ? `/${repo}/` : '',
 };
 
 export default nextConfig;
